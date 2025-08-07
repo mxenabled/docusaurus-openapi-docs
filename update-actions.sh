cb2903b21e740723d8407a4e1a948fe2c568455a
ashutosh-bhardwaj_mxtech <ashutosh.bhardwaj@mx.com>
2025-08-07 20:03:27 +0530

    fix: update all GitHub Actions to latest versions
    - Update compressed-size-action from v2 to v3 to resolve deprecated download-artifact@v3 error
    - Update all other actions to latest stable versions
    - Add update-actions.sh script for systematic action management

--- update-actions.sh
+++ update-actions.sh
@@ -20,6 +20,9 @@ find .github/workflows -name "*.yml" -o -name "*.yaml" | xargs sed -i '' 's/Fire
 # Update machine-learning-apps action from @master to specific version
 find .github/workflows -name "*.yml" -o -name "*.yaml" | xargs sed -i '' 's/machine-learning-apps\/actions-app-token@master/machine-learning-apps\/actions-app-token@v1/g'
 
+# Update compressed-size-action from v2 to v3
+find .github/workflows -name "*.yml" -o -name "*.yaml" | xargs sed -i '' 's/preactjs\/compressed-size-action@v2/preactjs\/compressed-size-action@v3/g'
+
 echo "Done updating GitHub Actions!"
 
 # Check for any remaining deprecated actions
