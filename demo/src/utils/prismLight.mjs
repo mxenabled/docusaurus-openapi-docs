67ae09e2e84bb76c210136a8169e829a8660c1b5
ashutosh-bhardwaj_mxtech <ashutosh.bhardwaj@mx.com>
2025-08-26 12:09:55 +0530

    fixing errors

--- demo/src/utils/prismLight.mjs
+++ demo/src/utils/prismLight.mjs
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import lightTheme from "prism-react-renderer/themes/github";
+import lightTheme from "prism-react-renderer/themes/github.js";
 
 export default {
   ...lightTheme,
